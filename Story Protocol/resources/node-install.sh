e91f5b0b8fbf58c820309451d6cff8daef9f22d3
Grand Valley <cobajohnson90@gmail.com>
2024-10-02 18:06:17 +0700

    update

--- Story Protocol/resources/node-install.sh
+++ Story Protocol/resources/node-install.sh
@@ -5,7 +5,6 @@ LOGO="
 /__ ._ _. ._   _|   \  / _. | |  _    
 \_| | (_| | | (_|    \/ (_| | | (/_ \/
                                     /
-
 "
 
 echo "$LOGO"
@@ -15,42 +14,22 @@ read -p "Enter your moniker: " MONIKER
 read -p "Enter your preferred port number: " STORY_PORT
 
 # 1. Install dependencies for building from source
-echo "Installing dependencies..."
-if sudo apt update -y && sudo apt upgrade -y && sudo apt install -y curl git jq build-essential gcc unzip wget lz4 openssl libssl-dev pkg-config protobuf-compiler clang cmake llvm llvm-dev; then
-    echo "Dependencies installed successfully."
-else
-    echo "Error installing dependencies."
-    exit 1
-fi
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y curl git jq build-essential gcc unzip wget lz4 openssl libssl-dev pkg-config protobuf-compiler clang cmake llvm llvm-dev
 
 # 2. Install Go
-echo "Installing Go..."
 cd $HOME && ver="1.22.0"
-if wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && rm "go$ver.linux-amd64.tar.gz"; then
-    echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bash_profile
-    source ~/.bash_profile
-    if go version; then
-        echo "Go installed successfully."
-    else
-        echo "Error installing Go."
-        exit 1
-    fi
-else
-    echo "Error downloading Go."
-    exit 1
-fi
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
+go version
 
 # 3. Install Cosmovisor
-echo "Installing Cosmovisor..."
-if go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest; then
-    echo "Cosmovisor installed successfully."
-else
-    echo "Error installing Cosmovisor."
-    exit 1
-fi
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
 
 # 4. Set environment variables
-echo "Setting environment variables..."
 echo "export MONIKER=\"$MONIKER\"" >> $HOME/.bash_profile
 echo "export STORY_CHAIN_ID=\"iliad\"" >> $HOME/.bash_profile
 echo "export STORY_PORT=\"$STORY_PORT\"" >> $HOME/.bash_profile
@@ -58,83 +37,47 @@ source $HOME/.bash_profile
 
 # 5. Download Geth and Consensus Client binaries
 cd $HOME
-echo "Downloading Geth binary..."
-if wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz; then
-    geth_folder_name=$(tar -tf geth-linux-amd64-0.9.3-b224fdf.tar.gz | head -n 1 | cut -f1 -d"/")
-    tar -xvf geth-linux-amd64-0.9.3-b224fdf.tar.gz
-    mv $HOME/$geth_folder_name/geth $HOME/go/bin/
-    sudo rm -rf $HOME/$geth_folder_name $HOME/geth-linux-amd64-0.9.3-b224fdf.tar.gz
-    echo "Geth binary downloaded and installed."
-else
-    echo "Error downloading Geth binary."
-    exit 1
-fi
 
-echo "Downloading Consensus Client binary..."
-if wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.9.13-b4c7db1.tar.gz; then
-    story_folder_name=$(tar -tf story-linux-amd64-0.9.13-b4c7db1.tar.gz | head -n 1 | cut -f1 -d"/")
-    tar -xzf story-linux-amd64-0.9.13-b4c7db1.tar.gz
-    mv $HOME/$story_folder_name/story $HOME/go/bin/
-    sudo rm -rf $HOME/$story_folder_name $HOME/story-linux-amd64-0.9.13-b4c7db1.tar.gz
-    echo "Consensus Client binary downloaded and installed."
-else
-    echo "Error downloading Consensus Client binary."
-    exit 1
-fi
+# Geth binary
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz
+geth_folder_name=$(tar -tf geth-linux-amd64-0.9.3-b224fdf.tar.gz | head -n 1 | cut -f1 -d"/")
+tar -xvf geth-linux-amd64-0.9.3-b224fdf.tar.gz
+mv $HOME/$geth_folder_name/geth $HOME/go/bin/
+sudo rm -rf $HOME/$geth_folder_name $HOME/geth-linux-amd64-0.9.3-b224fdf.tar.gz
+
+# Consensus client binary
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.9.13-b4c7db1.tar.gz
+story_folder_name=$(tar -tf story-linux-amd64-0.9.13-b4c7db1.tar.gz | head -n 1 | cut -f1 -d"/")
+tar -xzf story-linux-amd64-0.9.13-b4c7db1.tar.gz
+mv $HOME/$story_folder_name/story $HOME/go/bin/
+sudo rm -rf $HOME/$story_folder_name $HOME/story-linux-amd64-0.9.13-b4c7db1.tar.gz
 
 # 6. Initialize the app
-echo "Initializing the app..."
-if story init --network $STORY_CHAIN_ID --moniker $MONIKER; then
-    echo "App initialized successfully."
-else
-    echo "Error initializing the app."
-    exit 1
-fi
+story init --network $STORY_CHAIN_ID --moniker $MONIKER
 
 # 7. Add peers to the config.toml
-echo "Adding peers to the config..."
 peers=$(curl -sS https://lightnode-rpc-story.grandvalleys.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)
-if sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.story/story/config/config.toml; then
-    echo "Peers added successfully."
-else
-    echo "Error adding peers."
-    exit 1
-fi
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.story/story/config/config.toml
+echo $peers
 
 # 8. Set custom ports in config.toml
-echo "Setting custom ports..."
-if sed -i.bak -e "s%:26658%:${STORY_PORT}658%g;
+sed -i.bak -e "s%:26658%:${STORY_PORT}658%g;
 s%:26657%:${STORY_PORT}657%g;
 s%:26656%:${STORY_PORT}656%g;
-s%:26660%:${STORY_PORT}660%g" $HOME/.story/story/config/config.toml; then
-    echo "Ports set successfully."
-else
-    echo "Error setting custom ports."
-    exit 1
-fi
+s%:26660%:${STORY_PORT}660%g" $HOME/.story/story/config/config.toml
 
 # 9. Enable indexer (optional)
-echo "Enabling indexer..."
-if sed -i -e 's/^indexer = "null"/indexer = "kv"/' $HOME/.story/story/config/config.toml; then
-    echo "Indexer enabled."
-else
-    echo "Error enabling indexer."
-    exit 1
-fi
+sed -i -e 's/^indexer = "null"/indexer = "kv"/' $HOME/.story/story/config/config.toml
 
 # 10. Initialize Cosmovisor
-echo "Initializing Cosmovisor..."
-if cosmovisor init $HOME/go/bin/story; then
-    mkdir -p $HOME/.story/story/cosmovisor/upgrades
-    mkdir -p $HOME/.story/story/cosmovisor/backup
-    echo "Cosmovisor initialized successfully."
-else
-    echo "Error initializing Cosmovisor."
-    exit 1
-fi
+echo "export DAEMON_NAME=story" >> $HOME/.bash_profile
+echo "export DAEMON_HOME=$(find $HOME -type d -name "story")" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+cosmovisor init $HOME/go/bin/story
+mkdir -p $HOME/.story/story/cosmovisor/upgrades
+mkdir -p $HOME/.story/story/cosmovisor/backup
 
 # 11. Define Cosmovisor paths for the consensus client
-echo "Defining Cosmovisor paths..."
 input1=$(which cosmovisor)
 input2=$(find $HOME -type d -name "story")
 input3=$(find $HOME/.story/story/cosmovisor -type d -name "backup")
@@ -147,7 +90,6 @@ echo "Story home: $input2"
 echo "Backup directory: $input3"
 
 # 12. Create systemd service files for the consensus and Geth clients
-echo "Creating systemd service files..."
 
 # Consensus service file
 sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
@@ -191,11 +133,50 @@ WantedBy=multi-user.target
 EOF
 
 # 13. Start the node
-echo "Starting the node..."
 sudo systemctl daemon-reload
-if sudo systemctl enable story-geth story && sudo systemctl restart story-geth story; then
-    echo "Node installation and setup completed successfully!"
+sudo systemctl enable story-geth story
+sudo systemctl restart story-geth story
+
+# 14. Confirmation message for installation completion
+if systemctl is-active --quiet story && systemctl is-active --quiet story-geth; then
+    echo "Node installation and services started successfully!"
 else
-    echo "Error starting the node services."
-    exit 1
+    echo "Node installation failed. Please check the logs for more information."
 fi
+
+# 15. Version update to v0.10.0 (HARDFORK at height 626575)
+# Define the path of cosmovisor for being used in the consensus client
+input1=$(which cosmovisor)
+input2=$(find $HOME -type d -name "story")
+input3=$(find $HOME/.story/story/cosmovisor -type d -name "backup")
+echo "export DAEMON_NAME=story" >> $HOME/.bash_profile
+echo "export DAEMON_HOME=$input2" >> $HOME/.bash_profile
+echo "export DAEMON_DATA_BACKUP_DIR=$(find $HOME/.story/story/cosmovisor -type d -name "backup")" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+echo "input1: $input1"
+echo "input2: $input2"
+echo "input3: $input3"
+
+# Download the new node binary
+cd $HOME && \
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.10.0-9603826.tar.gz
+
+# Extract the new node binary
+story_folder_name=$(tar -tf story-linux-amd64-0.10.0-9603826.tar.gz | head -n 1 | cut -f1 -d"/")
+tar -xzf story-linux-amd64-0.10.0-9603826.tar.gz
+
+# Set access and delete the existing upgrade file in data dir
+sudo chown -R $USER:$USER $HOME/.story && sudo rm $HOME/.story/story/data/upgrade-info.json
+
+# Execute the cosmovisor `add-upgrade` command
+# v0.10.0 block height upgrade is 626575
+cosmovisor add-upgrade v0.10.0 $HOME/$story_folder_name/story --upgrade-height 626575 --force
+
+# After the instructions are successfully completed, delete the tar file and folder
+sudo rm -rf $HOME/$story_folder_name $HOME/story-linux-amd64-0.10.0-9603826.tar.gz
+
+# Confirmation that the node version update to v0.10.0 has been applied
+echo "Node auto-update to version v0.10.0 has been successfully applied at height 626575."
+
+# Reminder for the next update
+echo "Reminder: You must update the node version to v0.10.1 when the node reaches height 990454."
