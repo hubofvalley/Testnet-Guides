bb6ae2b652790b04433dbc25b612dedfa00bb4f6
Grand Valley <cobajohnson90@gmail.com>
2024-10-27 00:53:16 +0700

    update 0G auto

--- 0g (zero-gravity)/resources/0g_validator_node_install.sh
+++ 0g (zero-gravity)/resources/0g_validator_node_install.sh
@@ -9,9 +9,9 @@ LOGO="
 
 INTRO="
 Grand Valley's 0G public endpoints:
-- cosmos rpc: `https://lightnode-rpc-0g.grandvalleys.com`
-- json-rpc: `https://lightnode-json-rpc-0g.grandvalleys.com`
-- cosmos rest-api: `https://lightnode-api-0g.grandvalleys.com`
+- cosmos rpc: https://lightnode-rpc-0g.grandvalleys.com
+- json-rpc: https://lightnode-json-rpc-0g.grandvalleys.com
+- cosmos rest-api: https://lightnode-api-0g.grandvalleys.com
 
 0G Node Deployment Guide With Cosmovisor
 
@@ -23,9 +23,9 @@ System Requirements
 | Storage   | 1+ TB NVMe SSD                 |
 | Bandwidth | 100 MBps for Download / Upload |
 
-guide's current binaries version: `v0.2.5 will automatically update to the latest version`
-service file name: `0gchaind.service`
-current chain : `zgtendermint_16600-2`
+guide's current binaries version: v0.2.5 will automatically update to the latest version
+service file name: 0gchaind.service
+current chain : zgtendermint_16600-2
 "
 
 echo "$LOGO"
