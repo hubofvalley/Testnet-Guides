e0b61a1daaeeba78aeb57e3a1b1deeb3eea73137
Grand Valley <cobajohnson90@gmail.com>
2024-10-28 23:39:05 +0700

    done

---
+++ Story Protocol/resources/node-install_odyssey.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+LOGO="
+ __                                   
+/__ ._ _. ._   _|   \  / _. | |  _    
+\_| | (_| | | (_|    \/ (_| | | (/_ \/
+                                    /
+"
+
+echo "$LOGO"
+
+# Prompt for MONIKER and STORY_PORT
+read -p "Enter your moniker: " MONIKER
+read -p "Enter your preferred port number: " STORY_PORT
+
+# 1. Install dependencies for building from source
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y curl git jq build-essential gcc unzip wget lz4 openssl libssl-dev pkg-config protobuf-compiler clang cmake llvm llvm-dev
+
+# 2. Install Go
+cd $HOME && ver="1.22.0"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+# 3. Install Cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
+
+# 4. Set environment variables
+echo "export MONIKER=\"$MONIKER\"" >> $HOME/.bash_profile
+echo "export STORY_CHAIN_ID=\"odyssey\"" >> $HOME/.bash_profile
+echo "export STORY_PORT=\"$STORY_PORT\"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+# 5. Download Geth and Consensus Client binaries
+cd $HOME
+
+# Geth binary
+mkdir -p story-geth-v0.10.0
+wget -O story-geth-v0.10.0/geth-linux-amd64 https://github.com/piplabs/story-geth/releases/download/v0.10.0/geth-linux-amd64
+story_file__name=geth-linux-amd64
+cp story-geth-v0.10.0/$story_file__name $HOME/go/bin/geth
+sudo chown -R $USER:$USER $HOME/go/bin/geth
+sudo chmod +x $HOME/go/bin/geth
+
+# Consensus client binary
+mkdir -p story-v0.12.0
+wget -O story-v0.12.0/story-linux-amd64 https://github.com/piplabs/story/releases/download/v0.12.0/story-linux-amd64
+story_file__name=story-linux-amd64
+cp story-v0.12.0/$story_file__name $HOME/go/bin/story
+sudo chown -R $USER:$USER $HOME/go/bin/story
+sudo chmod +x $HOME/go/bin/story
+
+# 6. Initialize the app
+story init --network $STORY_CHAIN_ID --moniker $MONIKER
+
+# 7. Add peers to the config.toml
+peers=$(curl -sS https://lightnode-rpc-story.grandvalleys.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.story/story/config/config.toml
+echo $peers
+
+# 8. Set custom ports in config.toml
+sed -i.bak -e "s%:26658%:${STORY_PORT}658%g;
+s%:26657%:${STORY_PORT}657%g;
+s%:26656%:${STORY_PORT}656%g;
+s%:26660%:${STORY_PORT}660%g" $HOME/.story/story/config/config.toml
+
+# 9. Enable indexer (optional)
+sed -i -e 's/^indexer = "null"/indexer = "kv"/' $HOME/.story/story/config/config.toml
+
+# 10. Export Private key
+story validator export --evm-key-path $HOME/.story/story/config/private_key.txt --export-evm-key
+PRIVATE_KEY=$(grep -oP '(?<=PRIVATE_KEY=).*' $HOME/.story/story/config/private_key.txt)
+
+# 11. Initialize Cosmovisor
+echo "export DAEMON_NAME=story" >> $HOME/.bash_profile
+echo "export DAEMON_HOME=$(find $HOME -type d -name "story")" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+cosmovisor init $HOME/go/bin/story
+mkdir -p $HOME/.story/story/cosmovisor/upgrades
+mkdir -p $HOME/.story/story/cosmovisor/backup
+
+# 12. Define Cosmovisor paths for the consensus client
+input1=$(which cosmovisor)
+input2=$(find $HOME -type d -name "story")
+input3=$(find $HOME/.story/story/cosmovisor -type d -name "backup")
+echo "export DAEMON_NAME=story" >> $HOME/.bash_profile
+echo "export DAEMON_HOME=$input2" >> $HOME/.bash_profile
+echo "export DAEMON_DATA_BACKUP_DIR=$input3" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+echo "Cosmovisor path: $input1"
+echo "Story home: $input2"
+echo "Backup directory: $input3"
+
+# 13. Create systemd service files for the consensus and Geth clients
+
+# Consensus service file
+sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
+[Unit]
+Description=Cosmovisor Story Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+WorkingDirectory=$HOME/.story/story
+ExecStart=$input1 run run
+Restart=on-failure
+LimitNOFILE=65535
+Environment="DAEMON_NAME=story"
+Environment="DAEMON_HOME=$input2"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=true"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="DAEMON_DATA_BACKUP_DIR=$input3"
+Environment="UNSAFE_SKIP_BACKUP=true"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Geth service file
+sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
+[Unit]
+Description=Story Geth daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which geth) --odyssey --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 14. Start the node
+sudo systemctl daemon-reload
+sudo systemctl enable story-geth story
+sudo systemctl restart story-geth story
+
+# 14. Confirmation message for installation completion
+if systemctl is-active --quiet story && systemctl is-active --quiet story-geth; then
+    echo "Node installation and services started successfully!"
+else
+    echo "Node installation failed. Please check the logs for more information."
+fi
+
+# show the full logs
+echo "sudo journalctl -u story-geth -u story -fn 100"
---
+++ Story Protocol/resources/valleyofStory.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+LOGO="
+ __      __     _  _                        __    _____  _                       
+ \ \    / /    | || |                      / _|  / ____|| |                      
+  \ \  / /__ _ | || |  ___  _   _    ___  | |_  | (___  | |_  ___   _ __  _   _  
+  _\ \/ // _` || || | / _ \| | | |  / _ \ |  _|  \___ \ | __|/ _ \ | '__|| | | | 
+ | |\  /| (_| || || ||  __/| |_| | | (_) || |    ____) || |_| (_) || |   | |_| | 
+ | |_\/  \__,_||_||_| \___| \__, |  \___/ |_|   |_____/  \__|\___/ |_|    \__, | 
+ | '_ \ | | | |              __/ |                                         __/ | 
+ | |_) || |_| |             |___/                                         |___/  
+ |_.__/  \__, |                                                                  
+          __/ |                                                                  
+         |___/                                                                   
+ __                                   
+/__ ._ _. ._   _|   \  / _. | |  _    
+\_| | (_| | | (_|    \/ (_| | | (/_ \/
+                                    /
+"
+
+INTRO="
+Valley Of 0G by Grand Valley
+
+Story Validator Node System Requirements
+
+| Category  | Requirements     |
+| --------- | ---------------- |
+| CPU       | 8+ cores         |
+| RAM       | 32+ GB           |
+| Storage   | 500+ GB NVMe SSD |
+| Bandwidth | 100+ MBit/s         |
+
+- service file name: `story.service` `story-geth.service`
+- current chain: `odyssey`
+- current story node version: `v0.12.0`
+- current story-geth node version: `v0.10.0`
+
+"
+
+ENDPOINTS="
+Grand Valley Story Protocol public endpoints:
+- cosmos rpc: `https://lightnode-rpc-story.grandvalleys.com`
+- json-rpc: `https://lightnode-json-rpc-story.grandvalleys.com`
+- cosmos rest-api: `https://lightnode-api-story.grandvalleys.com`
+- cosmos ws: `wss://lightnode-rpc-story.grandvalleys.com/websocket`
+- evm ws: `wss://lightnode-wss-story.grandvalleys.com`
+
+Grand Valley social media:
+- X: https://x.com/bacvalley
+- GitHub: https://github.com/hubofvalley
+- Email: letsbuidltogether@grandvalleys.com
+"
+
+# Display LOGO and wait for user input to continue
+echo "$LOGO"
+echo -e "\nPress Enter to continue..."
+read -r
+
+# Display INTRO section and wait for user input to continue
+echo "$INTRO"
+echo -e "\nPress Enter to continue"
+read -r
+
+# Display ENDPOINTS section
+echo "$ENDPOINTS"
+
+# Validator Node Functions
+function deploy_validator_node() {
+    bash <(curl -s https://raw.githubusercontent.com/hubofvalley/Testnet-Guides/main/0g%20\(zero-gravity\)/resources/0g_validator_node_install.sh)
+    menu
+}
+
+function create_validator() {
+    read -p "Enter your private key: " PRIVATE_KEY
+    story validator create --stake 1000000000000000000 --private-key $(grep -oP '(?<=PRIVATE_KEY=).*' $HOME/.story/story/config/private_key.txt)
+    menu
+}
+
+function query_validator_pub_key() {
+    story validator export | grep -oP '(?<=Compressed Public Key (base64): ).*'
+    menu
+}
+
+function query_balance() {
+    echo "Select an option:"
+    echo "1. Query balance of your own EVM address"
+    echo "2. Query balance of another EVM address"
+    read -p "Enter your choice (1 or 2): " choice
+
+    case $choice in
+        1)
+            echo "Querying balance of your own EVM address..."
+            geth --exec "eth.getBalance('$(story validator export | grep -oP '(?<=EVM Public Key: ).*')')" attach $HOME/.story/geth/odyssey/geth.ipc
+            ;;
+        2)
+            read -p "Enter the EVM address to query: " evm_address
+            echo "Querying balance of $evm_address..."
+            geth --exec "eth.getBalance('$evm_address')" attach $HOME/.story/geth/odyssey/geth.ipc
+            ;;
+        *)
+            echo "Invalid choice. Please enter 1 or 2."
+            ;;
+    esac
+    menu
+}
+
+function stake_tokens() {
+    read -p "Enter validator pubkey: " VALIDATOR_PUBKEY
+    read -p "Enter amount to stake: " AMOUNT
+    story validator stake --validator-pubkey $VALIDATOR_PUBKEY --stake $AMOUNT  --private-key $(grep -oP '(?<=PRIVATE_KEY=).*' $HOME/.story/story/config/private_key.txt) -y
+    menu
+}
+
+function unstake_tokens() {
+    read -p "Enter validator pubkey: " VALIDATOR_PUBKEY
+    read -p "Enter amount to stake: " AMOUNT
+    story validator unstake --validator-pubkey $VALIDATOR_PUBKEY --stake $AMOUNT  --private-key $(grep -oP '(?<=PRIVATE_KEY=).*' $HOME/.story/story/config/private_key.txt)  -y
+    menu
+}
+
+function export_evm_private_key() {
+    story validator export --evm-key-path $HOME/.story/story/config/private_key.txt --export-evm-key
+    cat $HOME/.story/story/config/private_key.txt
+    menu
+}
+
+function export_evm_public_key() {
+    story validator export | grep -oP '(?<=EVM Public Key: ).*'
+    menu
+}
+
+function restore_wallet() {
+    read -p "Enter priv_validator_key: " PRIV_VALIDATOR_KEY
+    echo "$PRIV_VALIDATOR_KEY" > $HOME/.story/story/config/priv_validator_key.json
+    menu
+}
+
+function delete_validator_node() {
+    sudo systemctl stop story story-geth
+    sudo systemctl disable story story-geth
+    sudo rm -rf /etc/systemd/system/story.service
+    sudo rm -rf /etc/systemd/system/story-geth.service
+    sudo rm -rf $HOME/.story
+    sed -i "/STORY_/d" $HOME/.bash_profile
+    echo "Story Validator node deleted successfully."
+    menu
+}
+
+function show_consensus_client_logs() {
+    sudo journalctl -u story -fn 100
+    menu
+}
+
+function show_geth_logs() {
+    sudo journalctl -u story-geth -fn 100
+    menu
+}
+
+# Menu
+function menu() {
+    echo "1. Deploy Validator Node"
+    echo "2. Create Validator"
+    echo "3. Query Validator Public Key"
+    echo "4. Query Balance"
+    echo "5. Stake Tokens"
+    echo "6. Unstake Tokens"
+    echo "7. Export EVM Private Key"
+    echo "8. Export EVM Public Key"
+    echo "9. Restore Wallet"
+    echo "10. Delete Validator Node"
+    echo "11. Show Consensus Client Logs"
+    echo "12. Show Geth Logs"
+    echo "13. Exit"
+    read -p "Choose an option: " OPTION
+
+    case $OPTION in
+        1) deploy_validator_node ;;
+        2) create_validator ;;
+        3) query_validator_pub_key ;;
+        4) query_balance ;;
+        5) stake_tokens ;;
+        6) unstake_tokens ;;
+        7) export_evm_private_key ;;
+        8) export_evm_public_key ;;
+        9) restore_wallet ;;
+        10) delete_validator_node ;;
+        11) show_consensus_client_logs ;;
+        12) show_geth_logs ;;
+        13) exit 0 ;;
+        *) echo "Invalid option. Please try again." ;;
+    esac
+}
+
+# Run the menu
+while true; do
+    menu
+done
