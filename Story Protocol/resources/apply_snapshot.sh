ed4efc6ed7286c0eb182cc7202175b26dde2aaac
Grand Valley <cobajohnson90@gmail.com>
2024-11-05 11:47:57 +0700

    added ITRocket's snapshots

--- Story Protocol/resources/apply_snapshot.sh
+++ Story Protocol/resources/apply_snapshot.sh
@@ -11,7 +11,8 @@ NC='\033[0m' # No Color
 show_menu() {
     echo -e "${GREEN}Choose a snapshot provider:${NC}"
     echo "1. Mandragora"
-    echo "2. Exit"
+    echo "2. ITRocket"
+    echo "3. Exit"
 }
 
 # Function to choose snapshot type for Mandragora
@@ -37,6 +38,40 @@ choose_mandragora_snapshot() {
     esac
 }
 
+# Function to choose snapshot type for ITRocket
+choose_itrocket_snapshot() {
+    echo -e "${GREEN}Choose the type of snapshot for ITRocket:${NC}"
+    echo "1. Pruned"
+    echo "2. Archive"
+    read -p "Enter your choice: " snapshot_type_choice
+
+    case $snapshot_type_choice in
+        1)
+            GETH_SNAPSHOT_URL="https://server-1.itrocket.net/testnet/story/geth_story_2024-11-05_304590_snap.tar.lz4"
+            STORY_SNAPSHOT_URL="https://server-1.itrocket.net/testnet/story/story_2024-11-05_304590_snap.tar.lz4"
+            ;;
+        2)
+            GETH_SNAPSHOT_URL="https://server-5.itrocket.net/testnet/story/geth_story_2024-11-05_303734_snap.tar.lz4"
+            STORY_SNAPSHOT_URL="https://server-5.itrocket.net/testnet/story/story_2024-11-05_303734_snap.tar.lz4"
+            ;;
+        *)
+            echo -e "${RED}Invalid choice. Exiting.${NC}"
+            exit 1
+            ;;
+    esac
+}
+
+# Function to decompress snapshots
+decompress_snapshots() {
+    if [[ $GETH_SNAPSHOT_FILE == *.tar.lz4 ]]; then
+        lz4 -c -d $GETH_SNAPSHOT_FILE | tar -xv -C $HOME/.story/geth/odyssey/geth
+        lz4 -c -d $STORY_SNAPSHOT_FILE | tar -xv -C $HOME/.story/story
+    else
+        lz4 -c -d $GETH_SNAPSHOT_FILE | tar -xv -C $HOME/.story/geth/odyssey/geth
+        lz4 -c -d $STORY_SNAPSHOT_FILE | tar -xv -C $HOME/.story/story
+    fi
+}
+
 # Main script
 show_menu
 read -p "Enter your choice: " provider_choice
@@ -48,8 +83,17 @@ case $provider_choice in
         provider_name="Mandragora"
         echo -e "${GREEN}Grand Valley extends its gratitude to $provider_name for providing snapshot support.${NC}"
         choose_mandragora_snapshot
+        GETH_SNAPSHOT_FILE="geth_snapshot.lz4"
+        STORY_SNAPSHOT_FILE="story_snapshot.lz4"
         ;;
     2)
+        provider_name="ITRocket"
+        echo -e "${GREEN}Grand Valley extends its gratitude to $provider_name for providing snapshot support.${NC}"
+        choose_itrocket_snapshot
+        GETH_SNAPSHOT_FILE="geth_snapshot.tar.lz4"
+        STORY_SNAPSHOT_FILE="story_snapshot.tar.lz4"
+        ;;
+    3)
         echo -e "${GREEN}Exiting.${NC}"
         exit 0
         ;;
@@ -77,18 +121,17 @@ sudo cp $HOME/.story/story/data/priv_validator_state.json $HOME/.story/priv_vali
 sudo rm -rf $HOME/.story/geth/odyssey/chaindata $HOME/.story/story/data
 
 # Download story-geth and story snapshots in the background
-wget -O geth_snapshot.lz4 $GETH_SNAPSHOT_URL &
-wget -O story_snapshot.lz4 $STORY_SNAPSHOT_URL &
+wget -O $GETH_SNAPSHOT_FILE $GETH_SNAPSHOT_URL &
+wget -O $STORY_SNAPSHOT_FILE $STORY_SNAPSHOT_URL &
 
 # Wait for downloads to complete
 wait
 
 # Decompress story-geth and story snapshots
-lz4 -c -d geth_snapshot.lz4 | tar -xv -C $HOME/.story/geth/odyssey/geth
-lz4 -c -d story_snapshot.lz4 | tar -xv -C $HOME/.story/story
+decompress_snapshots
 
 # Delete downloaded story-geth and story snapshots
-sudo rm -v geth_snapshot.lz4 story_snapshot.lz4
+sudo rm -v $GETH_SNAPSHOT_FILE $STORY_SNAPSHOT_FILE
 
 # Restore your validator state
 sudo cp $HOME/.story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
