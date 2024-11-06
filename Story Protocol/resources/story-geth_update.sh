3c9fd1bd64d34f4381642d696f90d974b511557d
Grand Valley <cobajohnson90@gmail.com>
2024-11-06 22:40:46 +0700

    done

--- Story Protocol/resources/story-geth_update.sh
+++ Story Protocol/resources/story-geth_update.sh
@@ -10,14 +10,14 @@ update_version() {
 
     # Create directory and download the binary
     cd $HOME
-    mkdir -p $HOME/$version
-    if ! wget -P $HOME/$version $download_url/$geth_file_name -O $HOME/$version/geth; then
+    mkdir -p $HOME/story-geth-$version
+    if ! wget -P $HOME/story-geth-$version $download_url/$geth_file_name -O $HOME/story-geth-$version/geth; then
         echo "Failed to download the binary. Exiting."
         exit 1
     fi
 
     # Move the binary to the appropriate directory
-    sudo mv $HOME/$version/geth $HOME/go/bin/geth
+    sudo mv $HOME/story-geth-$version/geth $HOME/go/bin/geth
 
     # Set ownership and permissions
     sudo chown -R $USER:$USER $HOME/go/bin/geth
@@ -32,15 +32,15 @@ update_version() {
 echo "There is currently no latest version of story-geth available."
 
 # Menu for selecting the version
-# echo "Choose the version to update to:"
-# echo "There are currently no versions available."
+echo "Choose the version to update to:"
+echo "There are currently no versions available."
 
 # Placeholder for future versions
 # Uncomment and add new versions here
 # echo "a. v0.9.4"
 # echo "b. v0.9.5"
 
-# read -p "Enter the letter corresponding to the version: " choice
+read -p "Enter the letter corresponding to the version: " choice
 
 case $choice in
     # Placeholder for future versions
