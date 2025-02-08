42420a9ff6afe38a671e9f297bf74e87bb8b4629
Grand Valley <cobajohnson90@gmail.com>
2025-02-08 11:38:51 +0700

    change cometbft version to v0.37.15 on VoN

--- Namada-Campfire/resources/namada_validator_node_install_campfire_testnet.sh
+++ Namada-Campfire/resources/namada_validator_node_install_campfire_testnet.sh
@@ -63,7 +63,7 @@ cd $HOME
 rm -rf cometbft
 git clone https://github.com/cometbft/cometbft.git
 cd cometbft
-git checkout v0.37.11
+git checkout v0.37.15
 make build
 sudo cp $HOME/cometbft/build/cometbft /usr/local/bin/
 cometbft version
