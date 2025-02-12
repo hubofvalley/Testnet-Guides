dcb79c9642aef970b92b49b0fbf712d333ae0521
Grand Valley <cobajohnson90@gmail.com>
2025-02-13 01:58:29 +0700

    done

--- Story Protocol/resources/valleyofStory.sh
+++ Story Protocol/resources/valleyofStory.sh
@@ -73,7 +73,7 @@ Grand Valley Story Protocol public endpoints:${RESET}
 - cosmos rest-api: ${BLUE}https://lightnode-api-story.grandvalleys.com${RESET}
 - cosmos ws: ${BLUE}wss://lightnode-rpc-story.grandvalleys.com/websocket${RESET}
 - evm ws: ${BLUE}wss://lightnode-wss-story.grandvalleys.com${RESET}
-- peer: ${BLUE}fffb1a0dc2b6af331c65328c1ed9afad0bf107de@lightnode-peer-story.grandvalleys.com:26656${RESET}
+- peer: ${BLUE}267401ceb4815b8e7012b8f78e4268d57a84a5d6@lightnode-peer-story.grandvalleys.com:26656${RESET}
 
 ${GREEN}Connect with Story Protocol:${RESET}
 - Official Website: ${BLUE}https://www.story.foundation${RESET}
